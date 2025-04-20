734533ad6db0db1dfd30db50b7db6fe0034b7c83
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2025-04-20 12:59:27 +0200

    Finished defining endpoints

--- online-bookstore/online-bookstore-api-rest/src/main/java/es/pedrazamiguez/assessment/onlinebookstore/apirest/controller/InventoryController.java
+++ online-bookstore/online-bookstore-api-rest/src/main/java/es/pedrazamiguez/assessment/onlinebookstore/apirest/controller/InventoryController.java
@@ -5,10 +5,13 @@ import es.pedrazamiguez.assessment.onlinebookstore.domain.entity.BookAllocation;
 import es.pedrazamiguez.assessment.onlinebookstore.domain.usecase.inventory.AddToInventoryUseCase;
 import es.pedrazamiguez.assessment.onlinebookstore.domain.usecase.inventory.GetInventoryStatusUseCase;
 import es.pedrazamiguez.assessment.onlinebookstore.openapi.api.InventoryApi;
+import es.pedrazamiguez.assessment.onlinebookstore.openapi.model.AllocationDto;
 import es.pedrazamiguez.assessment.onlinebookstore.openapi.model.InventoryItemDto;
-import es.pedrazamiguez.assessment.onlinebookstore.openapi.model.InventoryRequestDto;
+
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,12 +25,18 @@ public class InventoryController implements InventoryApi {
 
   private final InventoryRestMapper inventoryRestMapper;
 
+  //  @Override
+  //  public ResponseEntity<Void> addBookCopiesToInventory(
+  //      final InventoryRequestDto inventoryRequestDto) {
+  //    this.addToInventoryUseCase.addToInventory(
+  //        inventoryRequestDto.getBookId(), inventoryRequestDto.getCopies());
+  //    return ResponseEntity.noContent().build();
+  //  }
+
   @Override
-  public ResponseEntity<Void> addBookCopiesToInventory(
-      final InventoryRequestDto inventoryRequestDto) {
-    this.addToInventoryUseCase.addToInventory(
-        inventoryRequestDto.getBookId(), inventoryRequestDto.getCopies());
-    return ResponseEntity.noContent().build();
+  public ResponseEntity<InventoryItemDto> addBookCopiesToInventory(
+      final Long bookId, final AllocationDto allocationDto) {
+    throw new NotImplementedException("Not implemented yet");
   }
 
   @Override
@@ -37,5 +46,15 @@ public class InventoryController implements InventoryApi {
     final List<InventoryItemDto> inventoryItemDtoList =
         this.inventoryRestMapper.toDtoList(bookAllocationFound);
     return ResponseEntity.ok(inventoryItemDtoList);
+  }
+
+  @Override
+  public ResponseEntity<Void> removeBookCopiesFromInventory(final Long bookId) {
+    throw new NotImplementedException("Not implemented yet");
+  }
+
+  @Override
+  public ResponseEntity<Void> removeOldBookCopies(final LocalDateTime date) {
+    throw new NotImplementedException("Not implemented yet");
   }
 }
