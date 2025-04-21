984e16abad0847bcf10bce0bf4a57274113e1ed0
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2025-04-21 15:37:01 +0200

    Fixed issue when retrieving current order

--- online-bookstore/online-bookstore-application/src/main/java/es/pedrazamiguez/assessment/onlinebookstore/application/usecase/order/ViewOrderUseCaseImpl.java
+++ online-bookstore/online-bookstore-application/src/main/java/es/pedrazamiguez/assessment/onlinebookstore/application/usecase/order/ViewOrderUseCaseImpl.java
@@ -1,13 +1,15 @@
 package es.pedrazamiguez.assessment.onlinebookstore.application.usecase.order;
 
 import es.pedrazamiguez.assessment.onlinebookstore.domain.entity.Order;
-import es.pedrazamiguez.assessment.onlinebookstore.domain.repository.OrderRepository;
+import es.pedrazamiguez.assessment.onlinebookstore.domain.service.order.CurrentOrderService;
+import es.pedrazamiguez.assessment.onlinebookstore.domain.service.order.FullPriceService;
 import es.pedrazamiguez.assessment.onlinebookstore.domain.service.security.SecurityService;
 import es.pedrazamiguez.assessment.onlinebookstore.domain.usecase.order.ViewOrderUseCase;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 
 @Component
 @RequiredArgsConstructor
@@ -15,11 +17,21 @@ public class ViewOrderUseCaseImpl implements ViewOrderUseCase {
 
   private final SecurityService securityService;
 
-  private final OrderRepository orderRepository;
+  private final CurrentOrderService currentOrderService;
+
+  private final FullPriceService fullPriceService;
 
   @Override
+  @Transactional
   public Optional<Order> getCurrentOrderForCustomer() {
     final String username = this.securityService.getCurrentUserName();
-    return this.orderRepository.findCreatedOrderForCustomer(username);
+    final Order existingOrder = this.currentOrderService.getOrCreateOrder(username);
+
+    if (CollectionUtils.isEmpty(existingOrder.getLines())) {
+      return Optional.empty();
+    }
+
+    this.fullPriceService.calculate(existingOrder);
+    return Optional.of(existingOrder);
   }
 }
