abad68599291ab154bcaeb8d5118aa78c20b0498
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2025-04-22 20:38:13 +0200

    Added tests for add book copies to inventory endpoint

--- online-bookstore/online-bookstore-api-rest/src/main/java/es/pedrazamiguez/assessment/onlinebookstore/apirest/exception/BookstoreExceptionHandler.java
+++ online-bookstore/online-bookstore-api-rest/src/main/java/es/pedrazamiguez/assessment/onlinebookstore/apirest/exception/BookstoreExceptionHandler.java
@@ -6,6 +6,7 @@ import es.pedrazamiguez.assessment.onlinebookstore.openapi.model.ErrorDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -22,34 +23,44 @@ public class BookstoreExceptionHandler {
   public ResponseEntity<ErrorDto> handleBookNotFoundException(
       final BookNotFoundException e, final WebRequest request) {
     final HttpStatus status = HttpStatus.NOT_FOUND;
-    return ResponseEntity.status(status).body(this.errorRestMapper.toDto(status, e, request));
+    return ResponseEntity.status(status)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(this.errorRestMapper.toDto(status, e, request));
   }
 
   @ExceptionHandler(BookAlreadyExistsException.class)
   public ResponseEntity<ErrorDto> handleBookAlreadyExistsException(
       final BookAlreadyExistsException e, final WebRequest request) {
     final HttpStatus status = HttpStatus.CONFLICT;
-    return ResponseEntity.status(status).body(this.errorRestMapper.toDto(status, e, request));
+    return ResponseEntity.status(status)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(this.errorRestMapper.toDto(status, e, request));
   }
 
   @ExceptionHandler(NotEnoughBookCopiesException.class)
   public ResponseEntity<ErrorDto> handleNotEnoughBookCopiesException(
       final NotEnoughBookCopiesException e, final WebRequest request) {
     final HttpStatus status = HttpStatus.CONFLICT;
-    return ResponseEntity.status(status).body(this.errorRestMapper.toDto(status, e, request));
+    return ResponseEntity.status(status)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(this.errorRestMapper.toDto(status, e, request));
   }
 
   @ExceptionHandler(BookNotInOrderException.class)
   public ResponseEntity<ErrorDto> handleBookNotInOrderException(
       final BookNotInOrderException e, final WebRequest request) {
     final HttpStatus status = HttpStatus.CONFLICT;
-    return ResponseEntity.status(status).body(this.errorRestMapper.toDto(status, e, request));
+    return ResponseEntity.status(status)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(this.errorRestMapper.toDto(status, e, request));
   }
 
   @ExceptionHandler(OrderContainsNoItemsException.class)
   public ResponseEntity<ErrorDto> handleOrderContainsNoItemsException(
       final OrderContainsNoItemsException e, final WebRequest request) {
     final HttpStatus status = HttpStatus.PRECONDITION_FAILED;
-    return ResponseEntity.status(status).body(this.errorRestMapper.toDto(status, e, request));
+    return ResponseEntity.status(status)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(this.errorRestMapper.toDto(status, e, request));
   }
 }
---
+++ online-bookstore/online-bookstore-api-rest/src/test/java/es/pedrazamiguez/assessment/onlinebookstore/apirest/controller/inventory/base/BaseInventoryTestData.java
@@ -0,0 +1,45 @@
+package es.pedrazamiguez.assessment.onlinebookstore.apirest.controller.inventory.base;
+
+import static org.instancio.Select.all;
+import static org.instancio.Select.field;
+
+import es.pedrazamiguez.assessment.onlinebookstore.domain.entity.Book;
+import es.pedrazamiguez.assessment.onlinebookstore.domain.entity.BookAllocation;
+import es.pedrazamiguez.assessment.onlinebookstore.openapi.model.AllocationDto;
+import es.pedrazamiguez.assessment.onlinebookstore.openapi.model.InventoryItemDto;
+import org.instancio.Instancio;
+
+public abstract class BaseInventoryTestData {
+
+  protected Long createRandomCopies() {
+    return Instancio.of(Long.class)
+        .supply(all(Long.class), gen -> gen.longRange(1L, 1000L))
+        .create();
+  }
+
+  protected AllocationDto givenAllocationDto(final Long copies) {
+    return Instancio.of(AllocationDto.class)
+        .supply(field(AllocationDto::getCopies), gen -> copies)
+        .create();
+  }
+
+  protected Book givenBook(final Long bookId) {
+    final Book book = Instancio.create(Book.class);
+    book.setId(bookId);
+    return book;
+  }
+
+  protected BookAllocation givenBookAllocation(final Long bookId, final Long copies) {
+    return Instancio.of(BookAllocation.class)
+        .supply(field(BookAllocation::getBook), gen -> this.givenBook(bookId))
+        .supply(field(BookAllocation::getCopies), gen -> copies)
+        .create();
+  }
+
+  protected InventoryItemDto givenInventoryItemDto(final Long bookId, final Long copies) {
+    return Instancio.of(InventoryItemDto.class)
+        .supply(field(InventoryItemDto::getBookId), gen -> bookId)
+        .supply(field(InventoryItemDto::getCopies), gen -> copies)
+        .create();
+  }
+}
