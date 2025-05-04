4228416f7c848538ac77368b4fb5c69c9624e823
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2025-05-04 18:25:58 +0200

    Added order controller tests

---
+++ online-bookstore/online-bookstore-api-rest/src/test/java/es/pedrazamiguez/assessment/onlinebookstore/apirest/controller/order/ClearCurrentOrderTest.java
@@ -0,0 +1,94 @@
+package es.pedrazamiguez.assessment.onlinebookstore.apirest.controller.order;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+
+import es.pedrazamiguez.assessment.onlinebookstore.apirest.controller.OrderController;
+import es.pedrazamiguez.assessment.onlinebookstore.apirest.controller.order.base.BaseOrderTestController;
+import es.pedrazamiguez.assessment.onlinebookstore.apirest.handler.BookstoreExceptionHandler;
+import es.pedrazamiguez.assessment.onlinebookstore.apirest.handler.RestExceptionHandler;
+import es.pedrazamiguez.assessment.onlinebookstore.apirest.mapper.ErrorRestMapper;
+import es.pedrazamiguez.assessment.onlinebookstore.domain.usecase.order.ClearOrderUseCase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.context.request.WebRequest;
+
+@ExtendWith(SpringExtension.class)
+class ClearCurrentOrderTest extends BaseOrderTestController {
+
+  private static final String CLEAR_CURRENT_ORDER_PATH = "/v1/orders/current";
+
+  @InjectMocks private OrderController orderController;
+
+  @InjectMocks private RestExceptionHandler restExceptionHandler;
+
+  @InjectMocks private BookstoreExceptionHandler bookstoreExceptionHandler;
+
+  @Mock private ClearOrderUseCase clearOrderUseCase;
+
+  @Mock private ErrorRestMapper errorRestMapper;
+
+  @BeforeEach
+  void setUp() {
+    super.setUp(this.orderController, this.bookstoreExceptionHandler, this.restExceptionHandler);
+  }
+
+  @Test
+  @DisplayName("DELETE " + CLEAR_CURRENT_ORDER_PATH + " returns HTTP 204 when order is cleared")
+  void shouldReturn204_whenOrderCleared() throws Exception {
+    // WHEN
+    final MvcResult result =
+        this.mockMvc
+            .perform(delete(CLEAR_CURRENT_ORDER_PATH).contentType(MediaType.APPLICATION_JSON))
+            .andReturn();
+
+    // THEN
+    assertThat(result.getResponse().getStatus()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    assertThat(result.getResponse().getContentAsString()).isEmpty();
+
+    verify(this.clearOrderUseCase).clearOrderItems();
+    verifyNoMoreInteractions(this.clearOrderUseCase);
+  }
+
+  @Test
+  @DisplayName(
+      "DELETE " + CLEAR_CURRENT_ORDER_PATH + " returns HTTP 500 when unexpected error occurs")
+  void shouldReturn500_whenUnexpectedErrorOccurs() throws Exception {
+    // GIVEN
+    final RuntimeException exception = new RuntimeException("Unexpected error");
+    doThrow(exception).when(this.clearOrderUseCase).clearOrderItems();
+
+    // WHEN
+    final MvcResult result =
+        this.mockMvc
+            .perform(delete(CLEAR_CURRENT_ORDER_PATH).contentType(MediaType.APPLICATION_JSON))
+            .andReturn();
+
+    // THEN
+    assertThat(result.getResponse().getStatus())
+        .isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+    assertThat(result.getResponse().getContentType())
+        .isEqualTo(MediaType.APPLICATION_JSON.toString());
+
+    verify(this.clearOrderUseCase).clearOrderItems();
+    verify(this.errorRestMapper)
+        .toDto(
+            eq(HttpStatus.INTERNAL_SERVER_ERROR),
+            any(RuntimeException.class),
+            any(WebRequest.class));
+    verifyNoMoreInteractions(this.clearOrderUseCase);
+  }
+}
---
+++ online-bookstore/online-bookstore-api-rest/src/test/java/es/pedrazamiguez/assessment/onlinebookstore/apirest/controller/order/base/BaseOrderTestData.java
@@ -0,0 +1,53 @@
+package es.pedrazamiguez.assessment.onlinebookstore.apirest.controller.order.base;
+
+import static org.instancio.Select.field;
+
+import es.pedrazamiguez.assessment.onlinebookstore.domain.model.Order;
+import es.pedrazamiguez.assessment.onlinebookstore.openapi.model.AllocationDto;
+import es.pedrazamiguez.assessment.onlinebookstore.openapi.model.OrderDto;
+import es.pedrazamiguez.assessment.onlinebookstore.openapi.model.PurchaseRequestDto;
+import java.util.Optional;
+import org.instancio.Instancio;
+
+public abstract class BaseOrderTestData {
+
+  protected Order givenOrder() {
+    return this.givenOrder(null);
+  }
+
+  protected Order givenOrder(final Long id) {
+    return Instancio.of(Order.class)
+        .generate(field("id"), gen -> gen.longs().min(id != null ? id : 1))
+        .set(field("id"), id)
+        .create();
+  }
+
+  protected OrderDto givenOrderDto() {
+    return this.givenOrderDto(null);
+  }
+
+  protected OrderDto givenOrderDto(final Long id) {
+    return Instancio.of(OrderDto.class)
+        .generate(field("id"), gen -> gen.longs().min(id != null ? id : 1))
+        .set(field("id"), id)
+        .create();
+  }
+
+  protected AllocationDto givenAllocationDto() {
+    return Instancio.of(AllocationDto.class)
+        .generate(field("copies"), gen -> gen.longs().range(1L, 100L))
+        .create();
+  }
+
+  protected PurchaseRequestDto givenPurchaseRequestDto() {
+    return Instancio.of(PurchaseRequestDto.class).create();
+  }
+
+  protected Optional<Order> givenOptionalOrder() {
+    return Optional.of(this.givenOrder());
+  }
+
+  protected Optional<Order> givenEmptyOptionalOrder() {
+    return Optional.empty();
+  }
+}
