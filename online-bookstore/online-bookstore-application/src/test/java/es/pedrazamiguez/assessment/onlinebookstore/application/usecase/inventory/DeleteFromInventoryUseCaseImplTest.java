aafa4f8d2ce9993753b12dd2473753d8d8bd2e80
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2025-05-05 07:53:03 +0200

    Added more tests

---
+++ online-bookstore/online-bookstore-application/src/test/java/es/pedrazamiguez/assessment/onlinebookstore/application/usecase/inventory/DeleteFromInventoryUseCaseImplTest.java
@@ -0,0 +1,63 @@
+package es.pedrazamiguez.assessment.onlinebookstore.application.usecase.inventory;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.instancio.Select.field;
+import static org.mockito.Mockito.*;
+
+import es.pedrazamiguez.assessment.onlinebookstore.domain.enums.BookCopyStatus;
+import es.pedrazamiguez.assessment.onlinebookstore.domain.model.Book;
+import es.pedrazamiguez.assessment.onlinebookstore.domain.model.BookAllocation;
+import es.pedrazamiguez.assessment.onlinebookstore.domain.repository.BookCopyRepository;
+import es.pedrazamiguez.assessment.onlinebookstore.domain.service.book.AvailableBookCopiesService;
+import java.util.Optional;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class DeleteFromInventoryUseCaseImplTest {
+
+  @InjectMocks private DeleteFromInventoryUseCaseImpl deleteFromInventoryUseCase;
+
+  @Mock private AvailableBookCopiesService availableBookCopiesService;
+
+  @Mock private BookCopyRepository bookCopyRepository;
+
+  @Test
+  void givenBookIdAndCopies_whenDeleteFromInventory_thenReturnBookAllocation() {
+    // GIVEN
+    final Long bookId = 1L;
+    final Long copies = 2L;
+
+    final BookAllocation bookAllocation =
+        Instancio.of(BookAllocation.class)
+            .supply(field(BookAllocation::getCopies), gen -> copies)
+            .supply(
+                field(BookAllocation::getBook),
+                gen -> Instancio.of(Book.class).supply(field(Book::getId), g -> bookId).create())
+            .supply(field(BookAllocation::getStatus), gen -> BookCopyStatus.DELETED)
+            .create();
+
+    when(this.bookCopyRepository.getInventoryDetailsByBookId(bookId))
+        .thenReturn(Optional.of(bookAllocation));
+
+    // WHEN
+    final Optional<BookAllocation> result =
+        this.deleteFromInventoryUseCase.deleteFromInventory(bookId, copies);
+
+    // THEN
+    assertThat(result).isPresent();
+    assertThat(result.get().getCopies()).isEqualTo(copies);
+    assertThat(result.get().getBook().getId()).isEqualTo(bookId);
+    assertThat(result.get().getStatus()).isEqualTo(BookCopyStatus.DELETED);
+
+    verify(this.availableBookCopiesService).assure(bookId, copies);
+    verify(this.bookCopyRepository).updateCopiesStatus(bookId, copies, BookCopyStatus.DELETED);
+    verify(this.bookCopyRepository).getInventoryDetailsByBookId(bookId);
+    verifyNoMoreInteractions(
+        this.availableBookCopiesService, this.bookCopyRepository, this.bookCopyRepository);
+  }
+}
---
+++ online-bookstore/online-bookstore-infrastructure/online-bookstore-repository/src/test/java/es/pedrazamiguez/assessment/onlinebookstore/repository/impl/CustomerRepositoryImplTest.java
@@ -0,0 +1,78 @@
+package es.pedrazamiguez.assessment.onlinebookstore.repository.impl;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.instancio.Select.field;
+import static org.mockito.Mockito.*;
+
+import es.pedrazamiguez.assessment.onlinebookstore.domain.model.Customer;
+import es.pedrazamiguez.assessment.onlinebookstore.repository.entity.CustomerEntity;
+import es.pedrazamiguez.assessment.onlinebookstore.repository.jpa.CustomerJpaRepository;
+import es.pedrazamiguez.assessment.onlinebookstore.repository.mapper.CustomerEntityMapper;
+import java.util.Optional;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CustomerRepositoryImplTest {
+
+  @InjectMocks private CustomerRepositoryImpl customerRepository;
+
+  @Mock private CustomerJpaRepository customerJpaRepository;
+
+  @Mock private CustomerEntityMapper customerEntityMapper;
+
+  @Test
+  void givenUsername_whenFindCustomerByUsername_thenReturnCustomer() {
+    // GIVEN
+    final String username = Instancio.create(String.class);
+    final CustomerEntity customerEntity =
+        Instancio.of(CustomerEntity.class)
+            .supply(field(CustomerEntity::getUsername), gen -> username)
+            .create();
+    final Customer customer =
+        Instancio.of(Customer.class).supply(field(Customer::getUsername), gen -> username).create();
+
+    when(this.customerJpaRepository.findByUsername(username))
+        .thenReturn(Optional.of(customerEntity));
+
+    when(this.customerEntityMapper.toDomain(customerEntity)).thenReturn(customer);
+
+    when(this.customerJpaRepository.findByUsername(username))
+        .thenReturn(Optional.of(customerEntity));
+
+    when(this.customerEntityMapper.toDomain(customerEntity)).thenReturn(customer);
+
+    // WHEN
+    final Optional<Customer> result = this.customerRepository.findCustomerByUsername(username);
+
+    // THEN
+    assertThat(result).isPresent();
+    assertThat(result).contains(customer);
+    assertThat(result.get().getUsername()).isEqualTo(username);
+
+    verify(this.customerJpaRepository).findByUsername(username);
+    verify(this.customerEntityMapper).toDomain(customerEntity);
+    verifyNoMoreInteractions(this.customerJpaRepository, this.customerEntityMapper);
+  }
+
+  @Test
+  void givenNonExistingUsername_whenFindCustomerByUsername_thenReturnEmpty() {
+    // GIVEN
+    final String username = Instancio.create(String.class);
+
+    when(this.customerJpaRepository.findByUsername(username)).thenReturn(Optional.empty());
+
+    // WHEN
+    final Optional<Customer> result = this.customerRepository.findCustomerByUsername(username);
+
+    // THEN
+    assertThat(result).isNotPresent();
+
+    verify(this.customerJpaRepository).findByUsername(username);
+    verifyNoMoreInteractions(this.customerJpaRepository);
+  }
+}
