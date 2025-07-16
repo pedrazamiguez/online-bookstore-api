d8d374020baa13d329f536a319c77c42acabb186
Andrés Pedraza Míguez <pedraza.miguez@gmail.com>
2025-07-16 13:08:03 +0200

    Added basic request logging config

---
+++ online-bookstore/online-bookstore-boot/src/main/java/es/pedrazamiguez/onlinebookstore/config/logging/RequestLoggingConfiguration.java
@@ -0,0 +1,22 @@
+package es.pedrazamiguez.onlinebookstore.config.logging;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingConfiguration {
+
+  @Bean
+  public CommonsRequestLoggingFilter requestLoggingFilter() {
+    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+    loggingFilter.setIncludeClientInfo(true);
+    loggingFilter.setIncludeHeaders(true);
+    loggingFilter.setIncludeQueryString(true);
+    loggingFilter.setIncludePayload(true);
+    loggingFilter.setMaxPayloadLength(10000);
+    loggingFilter.setAfterMessagePrefix("REQUEST DATA: ");
+    return loggingFilter;
+  }
+
+}
